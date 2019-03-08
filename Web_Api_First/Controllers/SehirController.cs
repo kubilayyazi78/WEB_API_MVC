f9115bb4baf0a119ac9852db33d87b1b482658a1
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-09 01:17:29 +0300

    .

---
+++ Web_Api_First/Controllers/SehirController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_Api_First.Controllers
+{
+    public class SehirController : ApiController
+    {//domain adresi/api/sehir
+
+        private string[] _sehirler= new string[] { "Antalya", "Karabük", "İstanbul" };
+        public string[] Get()
+        {
+            return _sehirler;
+        }
+
+        public string Get(int id)
+        {
+            return _sehirler[id];
+        }
+    }
+}

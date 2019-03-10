415c79153087f2fbbce417297b8e329d1c4f5bb6
kubilay yazı <kubilayyazi@hotmail.com>
2019-03-10 11:05:27 +0300

    form uygulamasından apideki  datalar çekildi.

---
+++ WebApi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:8091/");
+            HttpResponseMessage response = await client.GetAsync("api/sehir/2");
+            string result = await response.Content.ReadAsStringAsync();
+
+            label1.Text = result;
+
+        }
+    }
+}
